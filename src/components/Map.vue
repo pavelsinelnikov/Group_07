8407cc48da0e3e5fd6ad0917ca228418731d9fa9
pavelsinelnikov <pavel.sinelnikov@outlook.com>
2019-09-20 23:36:27 -0400

    Added map location retrieval

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,19 +1,54 @@
 <template>
   <div>
     <GmapMap
+      ref="mapRef"
       :center="{ lat: 10, lng: 10 }"
-      :zoom="7"
+      :zoom="2"
       map-type-id="terrain"
-      style="width: 500px; height: 300px"
-    ></GmapMap>
+      style="width: 1000px; height: 500px"
+    >
+      <GmapMarker
+        ref="mapMarker"
+        :position="marker.position"
+        :clickable="true"
+      ></GmapMarker>
+    </GmapMap>
   </div>
 </template>
 
 <script>
+import { gmapApi } from 'vue2-google-maps';
+
 export default {
   data() {
-    return {};
-  }
+    return {
+      map: null,
+      marker: {
+        position: {
+          lat: 0,
+          lng: 0
+        }
+      },
+      geocoder: null
+    };
+  },
+  computed: {
+    google: gmapApi
+  },
+  mounted() {
+    this.geocoder = new this.google.maps.Geocoder();
+    var that = this;
+    this.$refs.mapRef.$mapPromise.then(map => {
+      map.addListener('click', function(e) {
+        that.marker.position = e.latLng;
+        map.panTo(e.latLng);
+        that.geocoder.geocode({ latLng: e.latLng }, function(results, status) {
+          console.log(results[results.length - 1].formatted_address);
+        });
+      });
+    });
+  },
+  methods: {}
 };
 </script>
 
