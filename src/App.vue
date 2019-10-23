ca5d4a3b8a2fd49fbca11aa3ccb5c5862e969150
Andrew Tassone <andrewtassone@Andrews-MacBook-Pro.local>
2019-10-23 12:35:57 -0400

    Moved logo inside navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <Navbar></Navbar>
-    <img src="./assets/logo.png" />
     <router-view />
   </div>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,30 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="danger">
-      <b-container>
-        <b-navbar-brand href="/">NEWS MAP</b-navbar-brand>
-        <b-navbar-brand href="/contact">Contact</b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- SEARCH BAR IS NOT YET WORKING -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <!-- <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="WORK IN PROG"
-                v-model="country"
-              ></b-form-input> -->
-              <!-- <b-button
-                size="sm"
-                class="my-2 my-sm-0"
-                type="submit"
-                @click="fetchDataFromNewsAPI()"
-                >Search</b-button
-              > -->
-            </b-nav-form>
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template slot="button-content">
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-container>
-    </b-navbar>
+  <b-navbar toggleable="lg" type="dark" variant="danger">
+    <b-navbar-brand id="logo" href="#"><img src="../assets/logo.png"></b-navbar-brand>
+    <b-navbar-brand href="/">NEWS MAP</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="/contact">Contact</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="/login">Login</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
   </div>
 </template>
-
 <script>
 export default {
   name: 'Navbar',
@@ -59,4 +46,10 @@ export default {
   }
 };
 </script>
-<style></style>
+<style>
+
+#logo>img {
+  height: 70px;
+  width: auto;
+}
+  </style>
